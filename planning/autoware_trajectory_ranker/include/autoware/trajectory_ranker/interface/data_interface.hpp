7c5b1617b580cb96a0bcb0800e58d0c93ccc20f3
Zulfaqar Azmi <93502286+zulfaqar-azmi-t4@users.noreply.github.com>
2026-01-30 18:27:45 +0900

    fix(autoware_trajectory_ranker): prevent node crash when performing inner product operation (#12009)
    * fix: prevent not crash when performing inner product operation
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    * fix: missing code
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    * fix: wrong variable
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    ---------
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/autoware_trajectory_ranker/include/autoware/trajectory_ranker/interface/data_interface.hpp
+++ planning/autoware_trajectory_ranker/include/autoware/trajectory_ranker/interface/data_interface.hpp
@@ -54,7 +54,8 @@ public:
     for (size_t i = 0; i < metrics_.size() && i < weight.size(); i++) {
       const auto & w = weight.at(i);
       const auto & metric = metrics_.at(i);
-      scores_.at(i) = std::inner_product(w.begin(), w.end(), metric.begin(), 0.0f);
+      const size_t size = std::min(w.size(), metric.size());
+      scores_.at(i) = std::inner_product(w.begin(), w.begin() + size, metric.begin(), 0.0f);
     }
   }
 
