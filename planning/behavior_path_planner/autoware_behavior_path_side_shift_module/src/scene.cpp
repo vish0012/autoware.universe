7e218bdc5a5df438a9830c3a7b10e46992d35ebc
Taiki Yamada <129915538+TaikiYamada4@users.noreply.github.com>
2026-03-24 16:48:14 +0900

    fix: revert "fix(behavior_path_side_shift): preserve shifted path shap to prevent chattering (#12278)" (#12377)
    Revert "fix(behavior_path_side_shift): preserve shifted path shape to prevent chattering (#12278)"
    
    This reverts commit 1b4472c60fed4f83b1a1a9de1705af0465bec2b5.
    
    Signed-off-by: TaikiYamada4 <taiki.yamada@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_side_shift_module/src/scene.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_side_shift_module/src/scene.cpp
@@ -264,30 +264,20 @@ void SideShiftModule::replaceShiftLine()
 
 BehaviorModuleOutput SideShiftModule::plan()
 {
-  bool should_regenerate_shifted_path = false;
-
   // Replace shift line
   if (
     lateral_offset_change_request_ && ((shift_status_ == SideShiftStatus::BEFORE_SHIFT) ||
                                        (shift_status_ == SideShiftStatus::AFTER_SHIFT))) {
-    should_regenerate_shifted_path = true;
+    replaceShiftLine();
   } else if (shift_status_ != SideShiftStatus::BEFORE_SHIFT) {
     RCLCPP_DEBUG(getLogger(), "ego is shifting");
   } else {
     RCLCPP_DEBUG(getLogger(), "change is not requested");
   }
 
-  // Preserve the shape during shifting by reusing the last generated path unless re-generation
-  // is explicitly required.
+  // Refine path
   ShiftedPath shifted_path;
-  if (should_regenerate_shifted_path || prev_output_.path.points.empty()) {
-    if (should_regenerate_shifted_path) {
-      replaceShiftLine();
-    }
-    path_shifter_.generate(&shifted_path);
-  } else {
-    shifted_path = prev_output_;
-  }
+  path_shifter_.generate(&shifted_path);
 
   if (shifted_path.path.points.empty()) {
     RCLCPP_ERROR(getLogger(), "Generated shift_path has no points");
